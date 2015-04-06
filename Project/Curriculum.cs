7d38b2c7558079c2dc582b98f72773f82bf37a04
Devilquake - Joren Martens <joren.martens135@gmail.com>
2015-04-06 19:46:56 +0200

    More Added

---
+++ Project/Curriculum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Curriculum
+    {
+        //klasse leerstof!
+
+        //moeilijkheidsgraad: difficulty
+
+        //editing: aanpassen leerstof
+
+        //overzicht opvragen
+
+    }
+}
