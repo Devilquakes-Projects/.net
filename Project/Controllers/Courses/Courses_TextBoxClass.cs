298713998a4f05aca479436b5bd3d2dce180bf56
M-Gregory <gregory.malomgre@student.pxl.be>
2015-05-09 18:39:15 +0200

    Van klasses naar XAML (textboxsizes)

--- Project/Controllers/Courses/Courses_TextBoxClass.cs
+++ Project/Controllers/Courses/Courses_TextBoxClass.cs
@@ -17,17 +17,6 @@ namespace Project.Controllers.Courses
             : base(studentId, difficulty)
         { }//create a top-class object, so do nothing here
 
-        public void SetTextboxStartSize(double tbWidth = 200, double tbMaxWidth = 1.25, int amountOfCharactersAllowed = 20, TextBox tb1 = null, TextBox tb2 = null, TextBox tb3 = null, TextBox tb4 = null, TextBox tb5 = null, TextBox tb6 = null)//Author: Greg, Date: 23/04/15 16:30-18:30
-        {
-            TextBox[] tb = { tb1, tb2, tb3, tb4, tb5, tb6 };//Gewijzigd door Joren Martens, Date:???
-            for (int i = 0; i < tb.Length && tb[i] != null; i++)//Gewijzigd op 29/04 door Gregory
-            {
-                tb[i].MinWidth = tbWidth;
-                tb[i].MaxWidth = tbWidth * tbMaxWidth;
-                tb[i].MaxLength = amountOfCharactersAllowed;
-            }
-        }
-
         protected void LockTextBoxAndIsAnswerCorrect(TextBox tb, bool isCorrect)
         {
             tb.IsReadOnly = true;
